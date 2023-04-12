4060ca2c808b181241d0fe7e6f20e72119211aae
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-04-12 20:03:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1EX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variaveis
+            int b;
+            int h;
+            int a;
+            // Entrada de Dados
+            Console.Write("Informe a base: ");
+            b=int.Parse(Console.ReadLine());
+            Console.Write("Informea altura: ");
+            h=int.Parse(Console.ReadLine());
+            // Operação Aritmetica
+            a = b * h;
+            Console.WriteLine("O valor da área de um retângulo que tem base {0} e altura {1}, é {2}: ", b, h, a);
+            // Resultado
+            //Console.WriteLine(a);
+        }
+    }
+}
